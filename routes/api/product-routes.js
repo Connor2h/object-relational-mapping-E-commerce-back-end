de6ccaaf5ebe8866b0abb960f9a91c81cfc200cf
Connor Hollis <connorhollis3d@gmail.com>
2022-01-22 18:14:57 -0700

    updated get by id route and delete route in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -40,6 +40,29 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+
+  })
+  .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 // create new product
@@ -118,6 +141,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
